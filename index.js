67228f0ee4d3c0822fb1a3894626abdc55e2c6e5
Mustapha Ridwan <mustrid@Ridwanullah.localdomain>
2025-10-20 16:45:37 +0100

    Project setup with neccessary files and dependencies

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import env from 'dotenv'
+import cors from 'cors'
+import analyze from './routes/analyzerRoute.js'
+// configurations
+env.config()
+const app = express()
+const port = process.env.PORT
+
+//middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+// routes
+app.use('/',analyze)
+
+// Initillisation
+app.listen(port,()=>{
+  console.log(`App running on port-> ${port}`)
+})
